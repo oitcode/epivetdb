e29741b5cc6d9a3a67af17380c387f17cbd0b2e7
Shyam Sitaula <shyam.sitaula@oit.com.np>
2019-05-03 17:01:09 +0545

    First admin db seeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+use Illuminate\Support\Facades\DB;
+
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        /* Add first admin user to db. */
+        DB::table('users')->insert([
+            'name' => 'Admin One',
+            'email' => 'admin@epivetdb.gov.np',
+            'password' => bcrypt('epivetDbPass12#$');
+            'role' => 'admin',
+            'created_time' => \Carbon\Carbon::now(),
+            'updated_time' => \Carbon\Carbon::now(),
+        ]);
+    }
+}
