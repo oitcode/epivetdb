f08bfcbbb94d2a8381bdbae7011f7dc44407514a
Shyam Sitaula <shyam.sitaula@oit.com.np>
2019-05-03 17:11:16 +0545

    Not showing registration link in navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,7 +44,10 @@
                         <!-- Authentication Links -->
                         @guest
                             <li><a href="{{ route('login') }}">Login</a></li>
+                            {{--
+                            Do not show register link.
                             <li><a href="{{ route('register') }}">Register</a></li>
+                            --}}
                         @else
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true" v-pre>
