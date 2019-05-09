3603afdc316d85f27bbc4528e5f18246dc2e7d3c
Shyam Sitaula <shyam.sitaula@oit.com.np>
2019-05-09 15:34:25 +0545

    Created Status model

---
+++ app/Status.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Status extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'status';
+
+    /* Primary key */
+    protected $primayKey = 'status_id';
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | Relationship with user table.
+    |--------------------------------------------------------------------------
+    |
+    | Has many-to-one relationship with user table.
+    |
+    */
+
+    public function creator()
+    {
+        /**
+         * 2nd arg: name of foreign key column in status table
+         * 3rd arg: name of primary key column in user table
+         */
+        return $this->belongsTo('App\User', 'creator_id', 'id');
+    }
+}
--- app/User.php
+++ app/User.php
@@ -57,4 +57,9 @@ class User extends Authenticatable
     {
         return $this->hasMany('App\Disease', 'creator_id', 'id');
     }
+
+    public function statuses()
+    {
+        return $this->hasMany('App\Status', 'creator_id', 'id');
+    }
 }
